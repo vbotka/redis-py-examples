aa8cdc01463bc3625f3bc1daf982daf182d75182
Vladimir Botka <vbotka@gmail.com>
2017-11-29 18:03:16 +0100

    Links removed. Source-code preened.

--- docs/source/code/create-topchart.py
+++ docs/source/code/create-topchart.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # Tested with python 3.6.3, python-redis 2.10.5 and redis 4.0.1
 
+import redis
 import nltk
 from nltk.corpus import wordnet
 from nltk.tokenize import word_tokenize
-import redis
 # nltk.download()
 
 file = 'redis.txt'
@@ -13,12 +13,12 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 r.flushdb()
 
 f = open(file, 'r')
-
 text = f.read()
 words = word_tokenize(text)
 for word in words:
     if wordnet.synsets(word):
         r.zincrby("topchart", word, 1)
+f.close()
 
 ranking = r.zrange("topchart", 0, 10, 'DESC', 'WITHSCORES')
 for x in ranking:
